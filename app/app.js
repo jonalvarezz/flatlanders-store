cc72d3d974f1e53e3bc527e71f873d263803246a
Jonathan Alvarez González <jonas.al.co@gmail.com>
2014-10-04 18:21:44 -0500

    Gallery goes into module

--- app/app.js
+++ app/app.js
@@ -31,12 +31,20 @@
 		};
 	});
 
-	app.controller('GalleryController', function(){
-		this.current = 0;
+	app.directive('productGallery', function(){
+		return {
+			restrict: 'A',
+			templateUrl: 'partials/product-gallery.html',
+			controller: function() {
+				this.current = 0;
 
-		this.setCurrent = function(imageNumber){
-			this.current = imageNumber || 0;
+				this.setCurrent = function(imageNumber){
+					this.current = imageNumber || 0;
+				};
+			},
+			controllerAs: 'gallery'
 		};
+
 	});
 
 	app.controller('ReviewController', function(){
