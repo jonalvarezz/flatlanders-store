ba75d6c5abca83e4afff438406b9fb98d5c5e11a
Jonathan Alvarez González <jonas.al.co@gmail.com>
2014-10-04 15:59:00 -0500

    Initial structure from course

---
+++ app/app.js
@@ -0,0 +1,41 @@
+(function() {
+	'use strict';
+
+	var app = angular.module('gemStore', []);
+
+	app.controller('StoreController', function() {
+		// this.products = gems;
+	});
+
+	app.controller('TabController', function() {
+		this.tab = 1;
+
+		this.isSet = function(checkTab) {
+			return this.tab === checkTab;
+		};
+
+		this.setTab = function(setTab) {
+			this.tab = setTab;
+		};
+	});
+
+	app.controller('GalleryController', function(){
+		this.current = 0;
+
+		this.setCurrent = function(imageNumber){
+			this.current = imageNumber || 0;
+		};
+	});
+
+	app.controller('ReviewController', function(){
+
+		this.review = {};
+
+		this.addReview = function(product){
+			product.reviews.push(this.review);
+			this.review = {};
+		};
+
+	});
+
+})();
