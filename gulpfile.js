322e111a087aa123e053795db2ad1fcf35cdca4d
Jonathan Alvarez González <jonas.al.co@gmail.com>
2014-10-04 17:52:22 -0500

    including stylus support

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,52 @@
 'use strict';
 
-var gulp								= require('gulp'),
-		connect							= require('gulp-connect'),
-		historyApiFallback	= require('connect-history-api-fallback');
+var connect							= require('gulp-connect'),
+		gulp								= require('gulp'),
+		historyApiFallback	= require('connect-history-api-fallback'),
+		nib									= require('nib'),
+		rename							= require('gulp-rename'),
+		stylus							= require('gulp-stylus');
 
  // Create a server in port 8080
 gulp.task('server', function() {
 	connect.server({
 		root: 'app',
 		port: 8080,
+		hostname: '0.0.0.0',
+		livereload: true,
 		middleware: function(connect, opt) {
 			return [ historyApiFallback ];
 		}
 	});
 });
 
-gulp.task('default', [ 'server' ]);
+/*
+ * Assets processing and livereload enabling
+ * [css] : compile stylus files into css
+ * [html]: reload page
+*/
+
+// Process CSS files and reload the web browser
+gulp.task('css', function() {
+	gulp.src('./app/css/main.styl')
+		.pipe(stylus({
+			use: nib(),
+			compress: true
+		}))
+		.pipe(rename('main.min.css'))
+		.pipe(gulp.dest('./app/css/min/'))
+		.pipe(connect.reload());
+});
+// Reload the web browser when a HTML file changes
+gulp.task('html', function() {
+	gulp.src('./app/**/*.html')
+		.pipe(connect.reload());
+});
+
+// Watch file changes
+gulp.task('watch', function() {
+	gulp.watch(['./app/css/*.styl'], ['css']);
+	gulp.watch(['./app/**/*.html'], ['html']);
+});
+
+gulp.task('default', [ 'server', 'css', 'watch' ]);
